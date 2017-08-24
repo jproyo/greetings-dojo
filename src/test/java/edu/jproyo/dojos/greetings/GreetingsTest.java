a105db8a79f4c11408f405eac56a4e7684e6a2a1
Juan Pablo Royo <juanpablo.royo@gmail.com>
2017-08-24 19:08:01 +0200

    TDD advance

--- src/test/java/edu/jproyo/dojos/greetings/GreetingsTest.java
+++ src/test/java/edu/jproyo/dojos/greetings/GreetingsTest.java
@@ -3,17 +3,20 @@ package edu.jproyo.dojos.greetings;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Arrays;
+import java.util.Date;
 import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import edu.jproyo.dojos.greetings.adapter.email.EmailSenderAdapter;
 import edu.jproyo.dojos.greetings.adapter.email.SendResult;
 import edu.jproyo.dojos.greetings.dataaccess.EmployeeDataAccess;
+import edu.jproyo.dojos.greetings.model.Employee;
+import edu.jproyo.dojos.greetings.model.EmployeeResult;
 import edu.jproyo.dojos.greetings.services.notification.NotificationServiceImpl;
 import edu.jproyo.dojos.greetings.services.notification.strategy.EmailStrategy;
 
@@ -27,7 +30,6 @@ public class GreetingsTest {
 		EmailStrategy strategies = new EmailStrategy();
 		EmailSenderAdapter emailAdapter = Mockito.mock(EmailSenderAdapter.class);
 		strategies.setAdapter(emailAdapter);
-		Mockito.when(emailAdapter.sendEmail(Mockito.any())).thenReturn(SendResult.SENT);
 		NotificationServiceImpl notificationService = NotificationServiceImpl.create().withStrategy(strategies).build();
 		target = Greetings.create().notificationService(notificationService).buildWithDefaults();
 		GreetingResult result = target.sayGreetings();
@@ -54,9 +56,14 @@ public class GreetingsTest {
 		EmailStrategy strategies = new EmailStrategy();
 		EmailSenderAdapter emailAdapter = Mockito.mock(EmailSenderAdapter.class);
 		strategies.setAdapter(emailAdapter);
-		Mockito.when(emailAdapter.sendEmail(Mockito.any())).thenReturn(SendResult.NOT_SENT).thenReturn(SendResult.NOT_SENT);
+		Mockito.when(emailAdapter.sendEmail(Mockito.any(Employee.class))).thenReturn(SendResult.NOT_SENT);
 		NotificationServiceImpl notificationService = NotificationServiceImpl.create().withStrategy(strategies).build();
-		target = Greetings.create().notificationService(notificationService).buildWithDefaults();
+		EmployeeDataAccess employeeDataAccess = Mockito.mock(EmployeeDataAccess.class);
+		Employee employee = new Employee();
+		employee.setDateOfBirth(new Date());
+		EmployeeResult employeeResult = EmployeeResult.createFrom(Arrays.asList(employee));
+		Mockito.when(employeeDataAccess.loadEmployees()).thenReturn(Optional.ofNullable(employeeResult));
+		target = Greetings.create().notificationService(notificationService).employeeDataAccess(employeeDataAccess).buildWithDefaults();
 		GreetingResult result = target.sayGreetings();
 		assertNotNull(result);
 		assertEquals(GreetingResult.FAILED, result);
