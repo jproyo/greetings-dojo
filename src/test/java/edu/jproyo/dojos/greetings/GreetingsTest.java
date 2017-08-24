56cf6117048fb3d26cd8f39081794851418294f3
Juan Pablo Royo <juanpablo.royo@gmail.com>
2017-08-24 18:48:22 +0200

    TDD advance

--- src/test/java/edu/jproyo/dojos/greetings/GreetingsTest.java
+++ src/test/java/edu/jproyo/dojos/greetings/GreetingsTest.java
@@ -3,12 +3,16 @@ package edu.jproyo.dojos.greetings;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Optional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import edu.jproyo.dojos.greetings.adapter.email.EmailSenderAdapter;
+import edu.jproyo.dojos.greetings.dataaccess.EmployeeDataAccess;
 import edu.jproyo.dojos.greetings.services.notification.NotificationServiceImpl;
 import edu.jproyo.dojos.greetings.services.notification.strategy.EmailStrategy;
 
@@ -29,5 +33,30 @@ public class GreetingsTest {
 		GreetingResult result = target.sayGreetings();
 		assertNotNull(result);
 		assertEquals(GreetingResult.OK, result);
+	}
+	
+	@Test
+	public void testGreetingsWithNotFoundEmployees() {
+		EmailStrategy strategies = new EmailStrategy();
+		strategies.setAdapter(emailAdapter);
+		NotificationServiceImpl notificationService = NotificationServiceImpl.create().withStrategy(strategies).build();
+		EmployeeDataAccess employeeDataAccess = Mockito.mock(EmployeeDataAccess.class);
+		target = Greetings.create().notificationService(notificationService).employeeDataAccess(employeeDataAccess).buildWithDefaults();
+		Mockito.when(employeeDataAccess.loadEmployees()).thenReturn(Optional.empty());
+		GreetingResult result = target.sayGreetings();
+		assertNotNull(result);
+		assertEquals(GreetingResult.NOT_FOUND, result);
+	}
+	
+	@Test
+	public void testGreetingsWithFailingStrategies() {
+		EmailStrategy strategies = new EmailStrategy();
+		strategies.setAdapter(emailAdapter);
+		NotificationServiceImpl notificationService = NotificationServiceImpl.create().withStrategy(strategies).build();
+		Mockito.doThrow(new RuntimeException()).when(emailAdapter).sendEmail(Mockito.any());
+		target = Greetings.create().notificationService(notificationService).buildWithDefaults();
+		GreetingResult result = target.sayGreetings();
+		assertNotNull(result);
+		assertEquals(GreetingResult.FAILED, result);
 	}
 }
